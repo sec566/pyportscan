b6d0cb4da4b114ed54d80a76ae7ffdc38d8fa096
Abin Aboobacker <abinaboobacker.k@gmail.com>
2025-07-25 03:23:38 +0530

    Scanner modified saving result to a output file

--- BasicScannerSocket.py
+++ BasicScannerSocket.py
@@ -1,26 +1,33 @@
 import socket
 
-def scan_port(target,port):
+def scan_port(target,port,file):
     try: 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(1)
         result = s.connect_ex((target,port))
+
         if result == 0:
-            print(f"Port {port} is Open")
+            status = f"Port {port} is Open\n"
         elif result == 111 or result == 10061:
-            print(f"Port {port} is Closed")
+            status = f"Port {port} is Closed\n"
         else:
-            print(f"Port {port} is Filtered or Unreachable (code: {result})")
+            status = f"Port {port} is Filtered or Unreachable (code: {result})\n"
+
+        print(status)
+        file.write(status)
         s.close()
     except Exception as e:
-        print("Error in scanning port {port} : {e}")
+        error = f"Error in scanning port {port} : {e}"
+        print(error)
+        file.write(error)
 
 
 target = input("Enter Host name or  IP Address: ")
 try:
-    target = socket.gethostbyname(target)
+    target_ip = socket.gethostbyname(target)
 except socket.gaierror:
     print("Invalid Host ...!") 
+    exit()
     
 print("Choose scan mode :")
 print("1. Default ports :21,22,23,53,80,443,1433,3000,8080")
@@ -44,6 +51,8 @@ else:
 
 print(f"Scanning {target} \n")
 
-for port in ports:
-    scan_port(target,port)
+with open("Scan_result.txt","w") as f:
+    f.write(f"Scan result for {target}:\n\n")
+    for port in ports:
+        scan_port(target_ip,port,f)
         
